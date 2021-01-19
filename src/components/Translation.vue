87ee344b650630ff19fdb8973e636c3d817ef90d
Nicolas Durpoix <nicolas.durpoix82@gmail.com>
2021-01-19 21:10:21 +0100

    translation component

---
+++ src/components/Translation.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition-group name="fade">
+    <span :key="1">
+      <slot name="fr" v-if="store.lang === 'fr'" ></slot>
+    </span>
+    <span :key="2">
+      <slot name="en" v-if="store.lang === 'en'" ></slot>
+    </span>
+  </transition-group>
+</template>
+
+<script>
+import store from "../store/store.js";
+
+export default {
+  setup() {
+    return { store };
+  },
+};
+</script>
